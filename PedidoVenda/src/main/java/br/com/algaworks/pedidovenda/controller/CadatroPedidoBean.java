9736b8d10c3b0065332aab2764a03bb4f4c510bc
Vandame-PC <victorhugolgr@gmail.com>
2016-04-16 15:03:44 -0300

    Implementação do CDI

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadatroPedidoBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadatroPedidoBean.java
@@ -3,10 +3,10 @@ package br.com.algaworks.pedidovenda.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
 @RequestScoped
 public class CadatroPedidoBean {
 
