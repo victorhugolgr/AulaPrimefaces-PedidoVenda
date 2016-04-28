6e8d3c1412a13531d3d5874c15362598e0939101
Victor <victorhugolgr@gmail.com>
2016-04-28 17:06:15 -0300

    Adicionando restrições no pedido

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadatroPedidoBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadatroPedidoBean.java
@@ -1,22 +1,37 @@
 package br.com.algaworks.pedidovenda.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-@Named
-@RequestScoped
-public class CadatroPedidoBean {
+import br.com.algaworks.pedidovenda.model.Pedido;
 
+@Named
+@ViewScoped
+public class CadatroPedidoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Pedido pedido;
 	private List<Integer>  itens;
 
 	public CadatroPedidoBean(){
+		setPedido(new Pedido());
 		itens = new ArrayList<>();
                 itens.add(1);
 	}
 	
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
 	public List<Integer> getItens() {
 		return itens;
 	}
