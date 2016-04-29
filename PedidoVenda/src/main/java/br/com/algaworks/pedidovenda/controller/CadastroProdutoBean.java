19d2e35c831fd615aa9bd7a75a07feb976e44378
Victor <victorhugolgr@gmail.com>
2016-04-29 16:13:27 -0300

    Tentando gerar a lista de categoria na tela de produto

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,25 +1,40 @@
 package br.com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
+import br.com.algaworks.pedidovenda.model.Categoria;
 import br.com.algaworks.pedidovenda.model.Produto;
 
 @Named
 @ViewScoped
-public class CadastroProdutoBean implements Serializable{
+public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private EntityManager manager;
+
 	private Produto produto;
-	
+
+	private List<Categoria> categoriasRaizes;
+
 	public CadastroProdutoBean() {
 		produto = new Produto();
 	}
-	
-	public void salvar(){
+
+	public void inicializar() {
+		System.out.println("Inicializando...");
+
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+	}
+
+	public void salvar() {
 	}
 
 	public Produto getProduto() {
@@ -29,5 +44,13 @@ public class CadastroProdutoBean implements Serializable{
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	public void setCategoriasRaizes(List<Categoria> categoriasRaizes) {
+		this.categoriasRaizes = categoriasRaizes;
+	}
+
 }
