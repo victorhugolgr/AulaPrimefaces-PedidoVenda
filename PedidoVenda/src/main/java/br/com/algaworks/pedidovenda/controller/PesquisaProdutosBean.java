55da6de6470d65774e3e92870402cfb899bc05ff
Vandame-PC <victorhugolgr@gmail.com>
2016-05-01 16:50:55 -0300

    Implementando a pesquisa de produtos

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,26 +1,41 @@
 package br.com.algaworks.pedidovenda.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.algaworks.pedidovenda.model.Produto;
+import br.com.algaworks.pedidovenda.repository.ProdutoRepository;
+import br.com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
 
 @Named
 @RequestScoped
 public class PesquisaProdutosBean {
 
-	private List<Integer>  produtosFiltrados;
+	@Inject
+	private ProdutoRepository produtoRepository;
+
+	private ProdutoFilter filter;
+
+	private List<Produto> produtosFiltrados;
 
 	public PesquisaProdutosBean(){
-		produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		System.out.println("construtor lista produto");
+		filter = new ProdutoFilter();
 	}
 	
-	public List<Integer> getProdutosFiltrados() {
+	public void pesquisar() {
+		produtosFiltrados = produtoRepository.filtrados(filter);
+	}
+
+	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
-	
+
+	public ProdutoFilter getFilter() {
+		return filter;
+	}
+
 }
--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/repository/ProdutoRepository.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/repository/ProdutoRepository.java
@@ -1,32 +1,59 @@
 package br.com.algaworks.pedidovenda.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
 import br.com.algaworks.pedidovenda.model.Produto;
+import br.com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
 
 public class ProdutoRepository implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
-	private EntityManager entityManager;
+	private EntityManager manager;
 
 	public Produto guardar(Produto produto) {
-		return entityManager.merge(produto);
+		return manager.merge(produto);
 	}
 
 	public Produto proSku(String sku) {
 		try{			
-			return entityManager.createQuery("from Produto where upper(sku) = :sku",Produto.class)
+			return manager.createQuery("from Produto where upper(sku) = :sku",Produto.class)
 					.setParameter("sku", sku.toUpperCase())
 					.getSingleResult();
 		}catch(NoResultException e){
 			return null;
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Produto> filtrados(ProdutoFilter filtro){
+		
+		Session session = manager.unwrap(Session.class);
+		Criteria criteria = session.createCriteria(Produto.class);
+		
+		if(StringUtils.isNotBlank(filtro.getSku())){
+			criteria.add(Restrictions.eq("sku", filtro.getSku()));
+		}
+		
+		if(StringUtils.isNotBlank(filtro.getNome())){
+			//MatchMode.ANYWHERE coloca o % na query da pesquisa
+			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
+		}
+		
+		return criteria.addOrder(Order.asc("nome")).list();
 	}
 
 
