54dc0b7f5a06615d8e47fad2ab471c87ace5860f
Vandame-PC <victorhugolgr@gmail.com>
2016-05-10 00:32:18 -0300

    Implementações no cadastro de cliente

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadastroClienteBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/CadastroClienteBean.java
@@ -10,7 +10,6 @@ import javax.inject.Named;
 import br.com.algaworks.pedidovenda.model.Cliente;
 import br.com.algaworks.pedidovenda.model.Endereco;
 import br.com.algaworks.pedidovenda.model.TipoPessoa;
-import br.com.algaworks.pedidovenda.repository.ClienteRepository;
 import br.com.algaworks.pedidovenda.service.ClienteService;
 import br.com.algaworks.pedidovenda.util.jsf.FacesUtil;
 
@@ -21,22 +20,38 @@ public class CadastroClienteBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Cliente cliente;
-	private Endereco endereco = new Endereco();
-
-	@Inject
-	private ClienteRepository clienteRepository;
+	private Endereco endereco;
+	private Endereco enderecoSelecionado;
 	
+	public Endereco getEnderecoSelecionado() {
+		return enderecoSelecionado;
+	}
+
+	public void setEnderecoSelecionado(Endereco enderecoSelecionado) {
+		this.enderecoSelecionado = enderecoSelecionado;
+	}
+
 	@Inject
 	private ClienteService clienteService;
 
 	@PostConstruct
 	public void init() {
 		cliente = new Cliente();
+		endereco = new Endereco();
 	}
 
 	public void salvar() {
 		clienteService.salvar(cliente);
 		FacesUtil.addInfoMessage("Cliente cadastrado com sucesso!");
+	}
+	
+	public void salvarEndereco(){
+		cliente.getEnderecos().add(endereco);
+		endereco = new Endereco();
+	}
+	
+	public void excluirEndereco(){
+		cliente.getEnderecos().remove(enderecoSelecionado);
 	}
 
 	public TipoPessoa[] listaTipoPessoa() {
