0fe85963b49f37465d375cd97f67e3734e4b68a4
Vandame-PC <victorhugolgr@gmail.com>
2016-05-10 23:28:22 -0300

    Implementando a pesquisa de pedidos

--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/PesquisaPedidoBean.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/PesquisaPedidoBean.java
@@ -1,26 +1,50 @@
 package br.com.algaworks.pedidovenda.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.algaworks.pedidovenda.model.Pedido;
+import br.com.algaworks.pedidovenda.model.StatusPedido;
+import br.com.algaworks.pedidovenda.repository.PedidoRepository;
+import br.com.algaworks.pedidovenda.repository.filter.PedidoFilter;
+
 @Named
-@RequestScoped
-public class PesquisaPedidoBean {
+@ViewScoped
+public class PesquisaPedidoBean implements Serializable{
 
-	private List<Integer>  pedidosFiltrados;
-
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private PedidoRepository pedidoRepository;
+	
+	private PedidoFilter filtro;
+	private List<Pedido>  pedidosFiltrados;
+	
+	
 	public PesquisaPedidoBean(){
+		filtro = new PedidoFilter();
 		pedidosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			pedidosFiltrados.add(i);
-		}
 	}
 	
-	public List<Integer> getPedidosFiltrados() {
+	public void pesquisar(){
+		pedidosFiltrados = pedidoRepository.filtrados(filtro);
+	}
+	
+	public StatusPedido[] getStatus(){
+		return StatusPedido.values();
+	}
+	
+	public List<Pedido> getPedidosFiltrados() {
 		return pedidosFiltrados;
+	}
+
+	public PedidoFilter getFiltro() {
+		return filtro;
 	}
 	
 }
--- PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/model/StatusPedido.java
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/model/StatusPedido.java
@@ -2,5 +2,17 @@ package br.com.algaworks.pedidovenda.model;
 
 public enum StatusPedido {
 
-	ORCAMENTO, EMITIDO, CANCELADO
+	ORCAMENTO("Orçamento"), 
+	EMITIDO ("Emitido"), 
+	CANCELADO("Cancelado");
+	
+	private String descricao;
+	
+	StatusPedido(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao(){
+		return descricao;
+	}
 }
