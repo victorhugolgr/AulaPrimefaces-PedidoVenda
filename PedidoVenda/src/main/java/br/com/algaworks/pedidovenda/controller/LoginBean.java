10ed646659e5d7a8aba0c65126d67db5967296e5
victorhugolgr <victorhugolgr@gmail.com>
2016-07-28 23:11:38 -0300

    Criando uma página de login customizada

---
+++ PedidoVenda/src/main/java/br/com/algaworks/pedidovenda/controller/LoginBean.java
@@ -0,0 +1,61 @@
+package br.com.algaworks.pedidovenda.controller;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.algaworks.pedidovenda.util.jsf.FacesUtil;
+
+@Named
+@SessionScoped
+public class LoginBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private FacesContext facesContext;
+
+	@Inject
+	private HttpServletRequest request;
+
+	@Inject
+	private HttpServletResponse response;
+
+	private String email;
+
+	@PostConstruct
+	public void init() {
+		if (request.getParameter("invalid") != null && request.getParameter("invalid").equals("true")) {
+			System.out.println(request.getParameter("invalid"));
+			FacesUtil.addErrorMessage("Usuário ou senha inválido!");
+		}
+	}
+
+	public void login() throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/j_spring_security_check");
+		dispatcher.forward(request, response);
+
+		facesContext.responseComplete();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
